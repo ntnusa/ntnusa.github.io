79044450bd97d3170677c10f7539cab005cc4d23
wi1d5ky <wildsky@wildsky.cc>
2015-01-29 00:52:45 +0800

    地圖改用 open street map 支持開放資料社群！

--- contact.php
+++ contact.php
@@ -1,34 +1,34 @@
  <!DOCTYPE html>
 <html>
 	<head>
-		
+
 		<!--META TAGs-->
 		<meta name="Description" CONTENT="師大學生會的連絡資訊相關頁面。包含地址、地圖、電話、email、均可在本頁找到。"> <!-- 描述 -->
 		<meta name="KeyWords" content="師大學生會,NTNUSA,聯絡資訊">  <!-- 關鍵字, 協助SEO -->
 		<meta name="Author" CONTENT="學生會資訊部">  <!-- 作者 -->
 		<meta name="Generator" CONTENT="Notepad++">	 <!-- 生產的工具 -->
-		<meta http-equiv="Pragma" CONTENT="no_cache">  <!-- Cache -->		
+		<meta http-equiv="Pragma" CONTENT="no_cache">  <!-- Cache -->
 		<meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />  <!-- 網站文字編碼 -->
 		<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">   <!-- RWD -->
 		<link rev="made" href="https://www.facebook.com/NTNUstudentA"> <!-- 製作團隊 -->
 		<!--META TAGs-->
-		
+
 		<title>師大學生會．NTNUSA</title>
-		
+
 		<!-- bootstrap -->
 		<link href="css/bootstrap.css" rel="stylesheet">
 		<link href="css/bootstrap-responsive.css" rel="stylesheet">
-		
+
 		<link href="IMG/favicon.ico" type="image/x-icon" rel="shortcut icon">   <!-- title 前的圖片 -->
 		<link href="css/style.css" type="text/css" media="screen" rel="stylesheet"/>
 		<link href="css/nav_bar.css" type="text/css" media="screen" rel="stylesheet"/>
 		<link type="text/css" media="screen" rel="stylesheet" href="css/rwd_nav_bar.css"/>  <!-- rwd_nav_bar 樣式表 -->
-		
+
 		<!-- nav_bar 的javascript 相關程式碼  -->
 		<script src='javascript/jquery.min.js'></script>
 		<script type="text/javascript" src="javascript/nav_bar.js"></script>
 		<script type="text/javascript" src="javascript/nav_bar_function.js"></script>
-		
+
 	</head>
 	<style>
 @media only screen and (max-width: 720px) {
@@ -38,7 +38,7 @@
 	.content{
 		height:auto;
 	}
-	
+
 }
 @media only screen and (min-width: 720px) {
 	.BBODY{
@@ -48,7 +48,7 @@
 	.content{
 		height:830px;
 	}
-	
+
 	.content h2{
 		font-size:2em;
 		margin:0px;
@@ -62,25 +62,25 @@
 		padding:15px;
 		margin:30px 15px 30px 30px;
 	}
-		
+
 	.left li.list{
 		margin-left:-20px;
 		padding:10px 0px;
 		list-style:disc;
 	}
-	
+
 	.left ol li.list{
 		list-style:lower-alpha;
 	}
-	
+
 	.left ol{
 		padding-top:10px;
 	}
-	
+
 	.left ol li{
 		margin-left:0px;
 	}
-	
+
 	.right{
 		background:rgba(255,255,255,0.5);
 		width:485px;
@@ -89,7 +89,7 @@
 		padding:15px;
 		margin:30px 30px 30px 15px;
 	}
-	
+
 	div.map{
 		background:none;
 		height:430px;
@@ -128,18 +128,18 @@
 										<li>臺電大樓站：3號出口沿師大路往北直達。</li>
 									</ol>
 								</li>
-								
+
 								<li class="list">搭乘公車（By Bus）<br />
 									凡經「師大綜合大樓」、「師大」、「師大路」、「羅斯福師大路口」站之臺北市聯營公車皆可搭乘。
 									本會羅列搭乘路線如下：0南、1、15、18、74、208、235、236、237、251、252、254、
 									278、295、606、644、648、660、662、663、849、907、和平幹線、藍28。
 								</li>
-									
+
 								<li class="list">自行開車或計程車（By Car/Taxi）<br />
 									請依「辦公地址」查詢地圖，本會辦公室所在地之師大綜合大樓設有公共停車場及路邊停車格。
 								</li>
 							</ol>
-						
+
 						</li>
 					</ul>
 				</div>
@@ -147,8 +147,8 @@
 					<h2>我們在這裡!</h2>
 					<hr />
 					<div class="map">
-						<iframe src="http://goo.gl/wQNMhg" width="480" height="430" 
-							frameborder="0" style="border:0"></iframe>
+            <iframe width="480" height="430" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
+              src="https://www.openstreetmap.org/export/embed.html?bbox=121.52768790721893%2C25.026127102461516%2C121.53068125247955%2C25.02762421342213&amp;layer=mapnik&amp;marker=25.02687566022479%2C121.52918457984924"></iframe><br/><small><a href="https://www.openstreetmap.org/?mlat=25.02688&amp;mlon=121.52918#map=19/25.02688/121.52918">查看更大的地圖</a>，或是你是 <a href="https://www.google.com/maps/search/106台北市大安區和平東路一段129號+國立臺灣師範大學圖書館校區/@25.026715,121.528301,19z?hl=zh-TW">google map</a> 愛好者</small>
 						<div class="info">
 							<ul>
 								<li>Phone ：+886 2 7734 5337</li>
@@ -156,7 +156,7 @@
 								<li>Website：<a href="http://sa.sa.ntnu.edu.tw">http://sa.sa.ntnu.edu.tw</a></li>
 								<li>Facebook：<a href="http://fb.me/NTNUstudentA">http://fb.me/NTNUstudentA</a></li>
 								<li>Information Stand :	the third Tue  to Thur every month on Sunshine Ave</li>
-							</ul>	
+							</ul>
 						</div>
 					</div>
 				</div>
@@ -165,4 +165,4 @@
 		<?php include 'html/footer.html'; ?>
 	</center>
 	</body>
-</html> 
+</html>
