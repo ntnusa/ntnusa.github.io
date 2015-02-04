ddddae0ee8fc9d46f5aaea8ff9c591efcda1b051
wi1d5ky <wildsky@wildsky.cc>
2015-02-04 14:00:33 +0800

    bug fixing

--- javascript/nav_bar_function.js
+++ javascript/nav_bar_function.js
@@ -1,6 +1,7 @@
-if(window.matchMedia("(min-width: 700px)").matches) {
+
  $(function() {
   $('#sdt_menu > li').bind('mouseenter',function(){
+    if(window.matchMedia("(min-width: 700px)").matches) {
     var $elem = $(this);
     $elem.find('img').stop(true).animate({
                 'width': $elem.width(),
@@ -21,7 +22,9 @@ if(window.matchMedia("(min-width: 700px)").matches) {
               $sub_menu.show().animate({'left':left},200);
             }
           });
+      }
 	}).bind('mouseleave',function(){
+    if(window.matchMedia("(min-width: 700px)").matches) {
 		var $elem = $(this);
 		var $sub_menu = $elem.find('.sdt_box');
 		if($sub_menu.length)
@@ -39,6 +42,6 @@ if(window.matchMedia("(min-width: 700px)").matches) {
 			 .find('.sdt_wrap')
 			 .stop(true)
 			 .animate({'top':'-2px'},500); //the text go back
+     }
 	});
 });
-}
